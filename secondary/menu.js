13d7fd4f1279c5abf8a3fd455bb2e123ddb5e3a6
alligatorwooyo <wooyo.yuyao.j@gmail.com>
2024-10-27 20:50:34 -0400

    add secondary page

---
+++ secondary/menu.js
@@ -0,0 +1,9 @@
+function showmenu(){
+	document.getElementById('menuoverlay').style.display="block";
+	document.getElementById('topofpage').style.display="none ";
+}
+
+function hidemenu(){
+	document.getElementById('menuoverlay').style.display="none";
+	document.getElementById('topofpage').style.display="flex";
+}
